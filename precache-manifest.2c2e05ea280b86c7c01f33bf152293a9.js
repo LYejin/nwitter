020df32660652c88ba2435b403c6effe4c520dc5
LYejin <vgg789@naver.com>
2021-08-27 00:11:10 +0900

    Updates

---
+++ precache-manifest.2c2e05ea280b86c7c01f33bf152293a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e773f0dab71d9cb42b9ab5b1aa58dbf",
+    "url": "/LYejin/nwitter.git/index.html"
+  },
+  {
+    "revision": "04aa9c96f6b500a54cff",
+    "url": "/LYejin/nwitter.git/static/js/2.5eba5a2f.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/LYejin/nwitter.git/static/js/2.5eba5a2f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90197768501ea0a4df1b",
+    "url": "/LYejin/nwitter.git/static/js/main.1f6cd43a.chunk.js"
+  },
+  {
+    "revision": "0f19023a209a7496305d",
+    "url": "/LYejin/nwitter.git/static/js/runtime-main.223c9ee3.js"
+  }
+]);
